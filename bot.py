1543d1b0d0a7faff2e0077d8cad02207b344754f
kashincounter <bebra7275@gmail.com>
2024-08-05 16:41:10 +0300

    callback_data_fix

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import requests
 
 API_TOKEN = '7107117054:AAFjuZttOk1rfCOhodWhmbaR1_Z7l5owf4c'
 COINMARKETCAP_API_KEY = 'd3646ae5-77ca-4ff5-8fbf-aeb74fe051eb'
-COINMARKETCAP_API_URL = 'https://pro-api.coinmarketcap.com/v1/c>'
+COINMARKETCAP_API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -14,20 +14,22 @@ crypto_list = ['BTC', 'ETH', 'LTC', 'XRP', 'BCH']
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton("Избранные", callback_data='favourites')
-    btn2 = types.InlineKeyboardButton("Все криптовалюты", callback_data='all currencies')
+    btn1 = types.InlineKeyboardButton("Избранные", callback_data='favorites')
+    btn2 = types.InlineKeyboardButton("Все криптовалюты", callback_data='all_crypto')
     markup.add(btn1, btn2)
     bot.send_message(message.chat.id, "Выберите раздел:", reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.data == 'favorites':
-        bot.send_message(call.message.chat.id, "Вы выбрали раздел: Избранное")
+        bot.send_message(call.message.chat.id, "Вы выбрали раздел 'Избранные'.")
     elif call.data == 'all_crypto':
         markup = types.InlineKeyboardMarkup()
         for crypto in crypto_list:
             btn = types.InlineKeyboardButton(crypto, callback_data=crypto)
             markup.add(btn)
-        bot.send_message(call.message.chat.id, "Выберите криптовалюту")
-
+        bot.send_message(call.message.chat.id, "Выберите криптовалюту:", reply_markup=markup)
+    elif call.data in crypto_list:
+        pass 
+    
 bot.infinity_polling()
