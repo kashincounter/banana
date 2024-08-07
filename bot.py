23a1bf157f3847edab2715d9e49f32778ef54766
Sergayka <xuver@yandex.ru>
2024-08-08 01:37:41 +0300

    add db

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
+import cryptocompare
 import telebot
 from telebot import types
 import requests
 import json
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+import io
+
+from db import get_user_favorites, add_to_favorites
 
 API_TOKEN = '7107117054:AAFjuZttOk1rfCOhodWhmbaR1_Z7l5owf4c'
+COIN_STATS_API_KEY = "bTLH6MwYNtnDZJEZwrVcr4KQuVG9oWXQDKKfEMXjDck="
+CRYPTO_COMPARE_API_KEY = "bdf10bc646e114efccaf134bf6c5a9e36e8c10aa73ca28dbc0303645af16f0e6"
+cryptocompare.cryptocompare._set_api_key_parameter(CRYPTO_COMPARE_API_KEY)
+
 coins_url = "https://openapiv1.coinstats.app/coins/"
 
 headers = {
     "accept": "application/json",
-    "X-API-KEY": "bTLH6MwYNtnDZJEZwrVcr4KQuVG9oWXQDKKfEMXjDck="
+    "X-API-KEY": COIN_STATS_API_KEY
 }
 response = requests.get(coins_url, headers=headers).json()
 
@@ -32,11 +42,15 @@ def start(message):
 def callback_inline(call):
     # if call.data == 'favorites' or call.data == 'all_crypto':
     if call.data == 'favorites':
+        username = call.from_user.username
+        favourites = get_user_favorites(username)
+
         markup = types.InlineKeyboardMarkup(row_width=2)
-        for i in range(0, len(favourite_coins), 2):
-            btn1 = types.InlineKeyboardButton(favourite_coins[i], callback_data=favourite_coins[i])
-            if i + 1 < len(favourite_coins):
-                btn2 = types.InlineKeyboardButton(favourite_coins[i + 1], callback_data=favourite_coins[i + 1])
+
+        for i in range(0, len(favourites), 2):
+            btn1 = types.InlineKeyboardButton(favourites[i], callback_data=favourites[i])
+            if i + 1 < len(favourites):
+                btn2 = types.InlineKeyboardButton(favourites[i + 1], callback_data=favourites[i + 1])
                 markup.add(btn1, btn2)
             else:
                 markup.add(btn1)
@@ -64,17 +78,27 @@ def callback_inline(call):
         crypto_id = currencies_id[call.data]
         cryptocurrency = call.data
         price = get_price_id(crypto_id)
+        chart_image = generate_price_chart(crypto_id, cryptocurrency)
+
         back_btn = types.InlineKeyboardButton('<-Назад', callback_data='get_back')
         fav_btn = types.InlineKeyboardButton("Добавить в избранное ★",
                                              callback_data=f'add_to_favourite_{cryptocurrency}')
         markup.add(back_btn, fav_btn)
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                              text=f'1 {cryptocurrency} -> {round(price, 4)} USDT', reply_markup=markup)
+        # bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+        #                       text=f'1 {cryptocurrency} -> {round(price, 4)} USDT', reply_markup=markup)
+        if chart_image:
+            bot.send_photo(call.message.chat.id, chart_image, caption=f'1 {cryptocurrency} -> {round(price, 4)} USDT',
+                           reply_markup=markup)
+        else:
+            bot.send_message(call.message.chat.id, f'1 {cryptocurrency} -> {round(price, 4)} USDT', reply_markup=markup)
 
     elif call.data.startswith('add_to_favourite_'):
+        username = call.from_user.username
         cryptocurrency = call.data.split('_')[-1]
-        if cryptocurrency not in favourite_coins:
-            favourite_coins.append(cryptocurrency)
+        # if cryptocurrency not in favourite_coins:
+        #     favourite_coins.append(cryptocurrency)
+        add_to_favorites(username, cryptocurrency)
+
         bot.answer_callback_query(call.id, text=f"{cryptocurrency} добавлен в избранное!")
 
     elif call.data == 'get_back':
@@ -87,10 +111,53 @@ def callback_inline(call):
 
 
 def get_price_id(crypto_id):
-    price_url = f"https://openapiv1.coinstats.app/coins/{crypto_id}"
-    response = requests.get(price_url, headers=headers).json()
+    try:
+        price_url = f"https://openapiv1.coinstats.app/coins/{crypto_id}"
+        response = requests.get(price_url, headers=headers).json()
 
-    return response['price']
+        return response['price']
+
+    except Exception as e:
+        print(f"{e}")
+        return None
+
+
+def generate_price_chart(crypto_id, cryptocurrency):
+    try:
+        end_time = datetime.now()
+        start_time = end_time - timedelta(minutes=3)
+
+        historical_data = cryptocompare.get_historical_price_minute(
+            cryptocurrency, currency='USD', limit=12 * 3, toTs=int(end_time.timestamp())
+        )
+        print(historical_data)
+        if not historical_data:
+            print("Error fetching historical data.")
+            return None
+
+        #TODO: нужно сделать качественное отображение баров https://jenyay.net/Programming/Bar (почитай)
+
+        # times = [datetime.fromtimestamp(data['time']) for data in historical_data[::15]]
+        # values = [data['close'] for data in historical_data[::15]]
+        #
+        # plt.figure(figsize=(10, 5))
+        # plt.bar(times, values, width=0.01)
+        # plt.xlabel('Time')
+        # plt.ylabel('Price')
+        # plt.title(f'{cryptocurrency} Price (Last 3 hours)')
+        # plt.xticks(rotation=45)
+        # plt.tight_layout()
+        #
+        # image_stream = io.BytesIO()
+        # plt.savefig(image_stream, format='png')
+        # plt.close()
+        # image_stream.seek(0)
+        #
+        # return image_stream
+
+    except Exception as e:
+        print(f"Error generating chart: {e}")
+        return None
 
 
 bot.infinity_polling()
---
+++ db.py
@@ -0,0 +1,40 @@
+import psycopg2
+from psycopg2.extras import Json
+
+DB_HOST = "localhost"
+DB_NAME = "crypto_bot"
+DB_USER = ""
+DB_PASS = ""
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+cur = conn.cursor()
+
+
+def get_user_favorites(username):
+    try:
+        cur.execute("SELECT favorites FROM user_favorites WHERE username = %s", (username,))
+        result = cur.fetchone()
+        if result:
+            return result[0]
+        else:
+            return []
+    except Exception as e:
+        print(f"Error fetching user favorites: {e}")
+        return []
+
+
+def add_to_favorites(username, cryptocurrency):
+    try:
+        cur.execute("SELECT favorites FROM user_favorites WHERE username = %s", (username,))
+        result = cur.fetchone()
+        if result:
+            favorites = result[0]
+            if cryptocurrency not in favorites:
+                favorites.append(cryptocurrency)
+                cur.execute("UPDATE user_favorites SET favorites = %s WHERE username = %s", (favorites, username))
+        else:
+            favorites = [cryptocurrency]
+            cur.execute("INSERT INTO user_favorites (username, favorites) VALUES (%s, %s)", (username, favorites))
+        conn.commit()
+    except Exception as e:
+        print(f"Error adding to favorites: {e}")
